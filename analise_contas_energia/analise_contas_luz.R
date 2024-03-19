a25367b87656827997722b4873be233ce02c93b4
Leticia Faria <leticiaemanuella96@gmail.com>
2024-03-19 16:15:26 -0300

    correcao das labels eixo x e adicao de mais graficos

--- analise_contas_energia/analise_contas_luz.R
+++ analise_contas_energia/analise_contas_luz.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(openxlsx)
 
 # diretório de trabalho
-setwd("G:\Meu Drive\5. Cursos\programming\tools-functions\analise_contas_energia")
+setwd("G:/Meu Drive/5. Cursos/programming/tools-functions/analise_contas_energia")
 
 # limpar ambiente
 rm(list = ls())
@@ -13,8 +13,10 @@ rm(list = ls())
 contas_luz <- read.xlsx('contas_luz.xlsx')
 
 # tratamentos
-contas_luz <- contas_luz %>% 
-  unite("ano_mes", ano_vencimento:mes_vencimento, remove = FALSE)
+contas_luz <- contas_luz %>%
+  mutate(mes_vencimento_str = case_when(mes_vencimento < 10 ~ paste0("0", mes_vencimento),
+                                        TRUE ~ as.character(mes_vencimento))) %>% 
+  mutate(ano_mes = paste0(ano_vencimento, '_', mes_vencimento_str))
 
 # gráficos
 ggplot(contas_luz) +
@@ -23,7 +25,17 @@ ggplot(contas_luz) +
   geom_line(aes(group = 1)) +
   expand_limits(y = 0)
 
+ggplot(contas_luz) +
+  aes(x = ano_mes, y = valor_a_pagar) +
+  geom_point() +
+  geom_line(aes(group = 1)) +
+  expand_limits(y = 0)
 
+ggplot(contas_luz) +
+  aes(x = ano_mes, y = preco_atual) +
+  geom_point() +
+  geom_line(aes(group = 1)) +
+  expand_limits(y = 0)
 
 
 
