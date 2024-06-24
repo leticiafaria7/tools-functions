3d31b5a60e152157a3f338d52d135314d5471a82
Leticia Faria <leticiaemanuella96@gmail.com>
2024-06-24 17:58:44 -0300

    padronizar nomes de colunas

---
+++ python/padronizar_nomes_colunas.py
@@ -0,0 +1,21 @@
+import unicodedata
+import pandas as pd
+
+def remove_acentos(texto):
+	return ''.join((c for c in unicodedata.normalize('NFD', texto) if unicodedata.category(c) != 'Mn'))
+	
+def padronizar_nomes_colunas(df_):
+	
+    nomes_padronizados = []
+	
+    for coluna in df_.columns:
+        coluna = coluna.lower()
+        coluna = remove_acentos(coluna)
+        coluna = coluna.replace(' ', '_')
+        nomes_padronizados.append(coluna)
+		
+    return nomes_padronizados
+
+# exemplo de uso
+df = pd.DataFrame()
+df.columns = padronizar_nomes_colunas(df)
