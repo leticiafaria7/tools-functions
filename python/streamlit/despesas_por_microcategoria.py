619da3c75156430e71b02b898394f846b9122ade
Leticia Faria <leticiaemanuella96@gmail.com>
2024-07-28 17:04:12 -0300

    otimizacao da leitura da base

--- python/streamlit/despesas_por_microcategoria.py
+++ python/streamlit/despesas_por_microcategoria.py
@@ -4,6 +4,7 @@
 # adicionar legenda
 # adicionar outra visualização para cobrir o percentual
 # adicionar insights de onde dá para reduzir custos
+# adicionar gráfico de linhas com o valor absoluto e os percentuais dos gastos de cada macro
 
 
 import streamlit as st
@@ -18,27 +19,27 @@ from path_planilha import *
 # Carregar a base de dados    
 @st.cache_data
 def load_data():
-    # file_path = 'base_despesas.csv'
-    # if not os.path.exists(file_path):
-    #     st.error(f"Arquivo não encontrado: {file_path}")
-    #     st.stop()
+    file_path = PATH_DESPESAS
+    if not os.path.exists(file_path):
+        st.error(f"Arquivo não encontrado: {file_path}")
+        st.stop()
     return pd.read_excel(PATH_DESPESAS, sheet_name = 'despesas')
 
 df = load_data()
 df = df[df['categoria'].notna()]
 df = df[['categoria', 'valor', 'mes_num', 'mes', 'ano', 'macro']]
-# df['valor'] = df['valor'].astype(str).apply(lambda x: x.replace('R$ ', '').replace('.', '').replace(',', '.')).astype(float)
 df['mes_num'] = df['mes_num'].astype(int)
 df['ano'] = df['ano'].astype(int)
 
 # Criação das colunas para a interface
-st.subheader('Despesas por microcategoria')
+st.header('Análise de despesas')
 col1, col2 = st.columns([1, 2])  # 1: largura do menu, 2: largura do gráfico
 
 with col1:
     # Listas suspensas para seleção
     anos = df['ano'].unique()
-    meses = df['mes'].unique()
+    meses = list(df['mes'].unique())
+    meses.insert(0, 'todos os meses')
     opcoes_agrupamento = ['Sim', 'Não']
 
     ano_selecionado = st.selectbox('Selecione o ano', anos)
@@ -46,12 +47,16 @@ with col1:
     agrupamento_selecionado = st.selectbox('Agrupar categorias?', opcoes_agrupamento)
 
 # Filtrar os dados com base na seleção do usuário
-dados_filtrados = df[(df['ano'] == ano_selecionado) & (df['mes'] == mes_selecionado)].copy()
-dados_filtrados = dados_filtrados.groupby(['macro', 'categoria']).agg({'valor':'sum'}).reset_index()
-
+if mes_selecionado == 'todos os meses':
+    dados_filtrados = df[(df['ano'] == ano_selecionado)].copy()
+    dados_filtrados = dados_filtrados.groupby(['macro', 'categoria']).agg({'valor':'sum'}).reset_index()
+else:
+    dados_filtrados = df[(df['ano'] == ano_selecionado) & (df['mes'] == mes_selecionado)].copy()
+    dados_filtrados = dados_filtrados.groupby(['macro', 'categoria']).agg({'valor':'sum'}).reset_index()
 
 with col2:
     # Função para plotar o gráfico
+    st.markdown('##### Despesas por microcategoria')
     def plotar_grafico(df, agrupar):
 
         if agrupar == 'Sim':
