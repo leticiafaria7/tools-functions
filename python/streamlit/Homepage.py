e09b8a575d766e81e938959466d82a3ae5903b37
Leticia Faria <leticiaemanuella96@gmail.com>
2025-06-11 18:32:21 -0300

    grafico de barras emplihadas com cat ao longo do tempo e big numbers

--- python/streamlit/Homepage.py
+++ python/streamlit/Homepage.py
@@ -12,34 +12,85 @@ import pandas as pd
 import numpy as np
 import os
 import plotly.graph_objects as go
+from datetime import datetime
 
 from path_planilha import *
 
 st.set_page_config(layout="wide", page_title = 'Fluxo financeiro', page_icon = '🏠')
+
+def sep_milhar(num, casas_decimais = 0):
+    return f"{num:,.{casas_decimais}f}".replace(",", ".")
+
+# --------------------------------------------------------------------------------------------------------------------- #
+# Carregar e tratar base de dados
+# --------------------------------------------------------------------------------------------------------------------- #
+
+@st.cache_data
+def load_data():
+    file_path = PATH_DESPESAS
+    if not os.path.exists(file_path):
+        st.error(f"Arquivo não encontrado: {file_path}")
+        st.stop()
+    return pd.read_excel(file_path, sheet_name = 'despesas')
+
+df = load_data()
+df = df[df['categoria'].notna()]
+df = df[['categoria', 'valor', 'mes_num', 'mes', 'ano', 'macro']]
+df['mes_num'] = df['mes_num'].astype(int)
+df['ano'] = df['ano'].astype(int)
 
 # --------------------------------------------------------------------------------------------------------------------- #
 # Conteúdo da página
 # --------------------------------------------------------------------------------------------------------------------- #
 
 st.header('🏠 Fluxo financeiro')
+st.markdown('')
+st.markdown('##### Proporção de gastos por categoria ao longo do tempo')
+st.markdown('')
+tipo_grafico = st.selectbox('Tipo', ['Percentual', 'Valor'])
+st.markdown('')
 
-col1, col2 = st.columns(2)
-# if col1.button("Plain button", use_container_width=True):
-#     left.markdown("You clicked the plain button.")
-# if middle.button("Emoji button", icon="😃", use_container_width=True):
-#     middle.markdown("You clicked the emoji button.")
-# if right.button("Material button", icon=":material/mood:", use_container_width=True):
-#     right.markdown("You clicked the Material button.")
+# start_time = st.slider(
+#     "Selecionar intervalo de tempo",
+#     value=(datetime(2023, 1, 1), datetime(2025, 12, 31)),
+#     format="DD/MM/YYYY",
+# )
 
-#     st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+# st.write("Intervalo de tempo", start_time)
 
+barras_emplilhadas = df.copy()
+barras_emplilhadas['mes_ano'] = barras_emplilhadas['mes'] + '/' + barras_emplilhadas['ano'].astype(str)
+barras_emplilhadas['macro'] = np.where(barras_emplilhadas['macro'].isin(['streamings', 'estética', 'tarifas', 'contas', 'conhecimento']), 'outros', barras_emplilhadas['macro'])
+barras_emplilhadas = barras_emplilhadas.groupby(['mes_ano', 'ano', 'mes_num', 'macro'])['valor'].sum().reset_index()
+barras_emplilhadas['total_mes'] = barras_emplilhadas.groupby('mes_ano')['valor'].transform(sum)
+barras_emplilhadas['pct'] = barras_emplilhadas['valor'] / barras_emplilhadas['total_mes']
+barras_emplilhadas['macro'] = pd.Categorical(barras_emplilhadas['macro'],
+                                             categories = ['casa', 'investimentos', 'saúde', 'transporte', 'social',
+                                                           'eletrônicos', 'vestuário', 'alimentação', 'outros'],
+                                             ordered = True)
+barras_emplilhadas = barras_emplilhadas.sort_values(['macro', 'ano', 'mes_num', 'macro'])
+barras_emplilhadas['text'] = barras_emplilhadas['valor'].apply(lambda x: sep_milhar(x) + ',' + str(round(x, 2)).split('.')[1].ljust(2, '0'))
 
-with col1:
-    st.markdown('')
-    st.link_button("📊 Despesas por categoria", "Despesas_por_categoria", use_container_width = True)
-    st.link_button("📆 Despesas mensais", "Despesas_mensais", use_container_width = True)
+dict_cores = {'investimentos':'#2d2d2d', 'casa':'#3E5461', 'saúde':'#5288db', 'alimentação':'#364D37',
+                                 'social':'#B0BB9B', 'transporte':'#511012', 'eletrônicos':'#DFA03F', 'vestuário':'white', 'outros':'gray'}
 
-with col2:
-    st.markdown('')
-    st.link_button("📈 Microcategorias no tempo", "Microcategorias_no_tempo", use_container_width = True)
-    st.link_button("💰 Evolução dos investimentos", "Evolução_dos_investimentos", use_container_width = True)
+if tipo_grafico == 'Percentual':
+    eixo_y = 'pct'
+    fmt = '.0%'
+else:
+    eixo_y = 'valor'
+    fmt = '.0f'
+
+fig = go.Figure(layout = dict(barcornerradius = 5))
+for categoria in barras_emplilhadas['macro'].unique():
+    df_tmp = barras_emplilhadas[barras_emplilhadas['macro'] == categoria]
+    fig.add_trace(go.Bar(x = df_tmp['mes_ano'],
+                  y = df_tmp[eixo_y],
+                  name = categoria,
+                  marker_color = dict_cores[categoria],
+                  customdata = list(zip(df_tmp['macro'], df_tmp['text'], df_tmp['pct'])),
+                  hovertemplate = '%{customdata[0]}<br>Mês: %{x}<br>Total: R$ %{customdata[1]}<br>Percentual: %{customdata[2]:.1%}<extra></extra>'))
+    
+fig.update_layout(height = 530, barmode = 'stack', plot_bgcolor = 'rgba(0, 0, 0, 0)', paper_bgcolor = 'rgba(0, 0, 0, 0)', margin=dict(l=0, r=0, t=0, b=0))
+fig.update_yaxes(tickformat = fmt)
+st.plotly_chart(fig)
--- python/streamlit/pages/1_📊_Despesas_por_categoria.py
+++ python/streamlit/pages/1_📊_Despesas_por_categoria.py
@@ -20,18 +20,20 @@ def sep_milhar(num, casas_decimais = 0):
 # --------------------------------------------------------------------------------------------------------------------- #
 
 @st.cache_data
-def load_data():
+def load_data(sheet):
     file_path = PATH_DESPESAS
     if not os.path.exists(file_path):
         st.error(f"Arquivo não encontrado: {file_path}")
         st.stop()
-    return pd.read_excel(file_path, sheet_name = 'despesas')
+    return pd.read_excel(file_path, sheet_name = sheet)
 
-df = load_data()
+df = load_data('despesas')
 df = df[df['categoria'].notna()]
 df = df[['categoria', 'valor', 'mes_num', 'mes', 'ano', 'macro']]
 df['mes_num'] = df['mes_num'].astype(int)
 df['ano'] = df['ano'].astype(int)
+
+df_entradas = load_data('entradas_transferencias')
 
 # --------------------------------------------------------------------------------------------------------------------- #
 # Criar colunas para as listas suspensas
@@ -40,7 +42,8 @@ df['ano'] = df['ano'].astype(int)
 st.header('📊 Despesas por categoria')
 
 row1 = st.columns([1, 2, 2, 3])
-row2 = st.columns([1, 1])
+row2 = st.columns([1, 1, 1])
+row3 = st.columns([1, 1], gap = 'medium')
 
 # --------------------------------------------------------------------------------------------------------------------- #
 # Menu de listas suspensas
@@ -112,10 +115,48 @@ dados_filtrados_treemap['text'] = dados_filtrados_treemap['valor'].apply(lambda 
 dados_filtrados_treemap['text'] = 'R$ ' + dados_filtrados_treemap['text'].round(2).astype(str) + '</b> (' + dados_filtrados_treemap['pct'].astype(str).apply(lambda x: x.split('.')[0]) + '%)'
 
 # --------------------------------------------------------------------------------------------------------------------- #
+# Big numbers
+# --------------------------------------------------------------------------------------------------------------------- #
+
+if mes_selecionado == 'todos os meses':
+    total_entradas = df_entradas[(df_entradas['ano'] == ano_selecionado) & (df_entradas['banco de origem'].isna())]
+    total_saidas = df[(df['ano'] == ano_selecionado)]
+else:
+    total_entradas = df_entradas[(df_entradas['ano'] == ano_selecionado) & (df_entradas['mes'] == mes_selecionado) & (df_entradas['banco de origem'].isna())]
+    total_saidas = df[(df['ano'] == ano_selecionado) & (df['mes'] == mes_selecionado)]
+
+with row2[0]:
+    total_entradas = total_entradas['valor'].sum()
+    total_entradas_trat = 'R$ ' + str(sep_milhar(round(total_entradas, 0))) + ',' + str(round(total_entradas, 2)).split('.')[1].ljust(2, '0')
+
+    st.text('')
+    st.markdown(f'''
+                ##### :green[{total_entradas_trat}]
+                :green[Total entradas]''')
+    
+with row2[1]:
+    total_saidas = total_saidas['valor'].sum()
+    total_saidas_trat = 'R$ ' + str(sep_milhar(round(total_saidas, 0))) + ',' + str(round(total_saidas, 2)).split('.')[1].ljust(2, '0')
+
+    st.text('')
+    st.markdown(f'''
+                ##### :red[{total_saidas_trat}]
+                :small[:red[Total saídas]]''')
+
+with row2[2]:
+    sobra = total_entradas - total_saidas
+    sobra = 'R$ ' + str(sep_milhar(round(sobra, 0))) + ',' + str(round(sobra, 2)).split('.')[1].ljust(2, '0')
+
+    st.text('')
+    st.markdown(f'''
+                ##### :gray[{sobra}]
+                :gray[Sobra]''')
+
+# --------------------------------------------------------------------------------------------------------------------- #
 # Gráficos
 # --------------------------------------------------------------------------------------------------------------------- #
 
-with row2[0]:
+with row3[0]:
 
     st.text("")
     st.text("")
@@ -134,7 +175,7 @@ with row2[0]:
                             hovertemplate = '%{y}<br><br>Total gasto: R$ %{customdata[1]}<br>Proporção: %{customdata[0]}<extra></extra>',
                             text = df['text']))
         fig.add_shape(type = 'line', x0 = 0, x1 = 0, y0 = -1, y1 = df.shape[0], line = dict(width = 0.8, color = 'lightgray'))
-        fig.update_layout(plot_bgcolor = 'rgba(0, 0, 0, 0)', paper_bgcolor = 'rgba(0, 0, 0, 0)', height = 550, width = 800, 
+        fig.update_layout(plot_bgcolor = 'rgba(0, 0, 0, 0)', paper_bgcolor = 'rgba(0, 0, 0, 0)', height = 450, width = 800, 
                           margin=dict(l=0, r=0, t=0, b=0))
         fig.update_xaxes(range = [0, df.valor.max() * 1.3])
         
@@ -147,8 +188,8 @@ with row2[0]:
         fig.add_trace(go.Treemap(labels = df['macro'], values = df['valor'], parents = parents, text = df['text'],
                                 textinfo = "label+text",
                                 hovertemplate='%{label}<br>%{text}<extra><extra>'))
-        fig.update_layout(plot_bgcolor = 'rgba(0, 0, 0, 0)', paper_bgcolor = 'rgba(0, 0, 0, 0)', height = 550, width = 800, 
-                          margin=dict(l=0, r=0, t=0, b=0), treemapcolorway = ['brown'])
+        fig.update_layout(plot_bgcolor = 'rgba(0, 0, 0, 0)', paper_bgcolor = 'rgba(0, 0, 0, 0)', height = 450, width = 800, 
+                          margin=dict(l=0, r=0, t=0, b=0), treemapcolorway = ['gray'])
         fig.update_traces(marker=dict(cornerradius=5))
         st.plotly_chart(fig)
 
@@ -157,7 +198,7 @@ with row2[0]:
     else:
         plotar_grafico_treemap(dados_filtrados_treemap)
 
-with row2[1]:
+with row3[1]:
     
     st.text("")
     st.text("")
@@ -176,7 +217,7 @@ with row2[1]:
                             hovertemplate = '%{y}<br><br>Total gasto: R$ %{customdata[1]}<br>Proporção: %{customdata[0]}%<extra></extra>',
                             text = df['text']))
         fig.add_shape(type = 'line', x0 = 0, x1 = 0, y0 = -1, y1 = df.shape[0], line = dict(width = 0.8, color = 'lightgray'))
-        fig.update_layout(plot_bgcolor = 'rgba(0, 0, 0, 0)', paper_bgcolor = 'rgba(0, 0, 0, 0)',  height = 250, width = 800,
+        fig.update_layout(plot_bgcolor = 'rgba(0, 0, 0, 0)', paper_bgcolor = 'rgba(0, 0, 0, 0)',  height = 150, width = 800,
                           margin=dict(l=0, r=0, t=0, b=0))
         fig.update_xaxes(range = [0, df.valor.max() * 1.3])
         st.plotly_chart(fig)
